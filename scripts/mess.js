24bd3ddb88a77a3b735c368f5e6b0f1356a9c9a4
Eugene-Ba <eugenekotov.email@gmail.com>
2020-11-17 03:21:24 +0300

    Update mess.js

--- scripts/mess.js
+++ scripts/mess.js
@@ -1,8 +1,10 @@
 function mess(text) {
   let mess = document.querySelector(".mess");
   mess.innerHTML = text;
+  mess.style.marginTop = '';
   mess.style.transition = "1s opacity";
   mess.style.opacity = "1";
   setTimeout("document.querySelector('.mess').style.opacity = '0'", 2500);
   setTimeout("document.querySelector('.mess').style.transition = ''", 2550);
+  setTimeout("document.querySelector('.mess').style.marginTop = '-600px'", 3400);
 }
